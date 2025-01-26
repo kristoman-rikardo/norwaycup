a7f4bb90c85053ae4a3ffeb14fb5701e0d6e2262
kristoman-rikardo <kristofferswik@gmail.com>
2025-01-26 09:38:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -158,24 +158,21 @@ const FormExtension = {
   };  
 // Last inn Chat-widget
 let script = document.createElement("script");
-script.onload = function() {
-  window.voiceflow.chat.load({
-    verify: { projectID: '67436de76414aa7986e35112' },
-    url: 'https://general-runtime.voiceflow.com',
-    versionID: 'production',
-    allowDangerousHTML: true,
-    assistant: {
-      description: widgetDescription,
-      extensions: [FormExtension],
-      stylesheet: widgetStyleSheet,
-    }
-  }).then(() => {
-    console.log('Voiceflow widget loaded successfully');
-    window.voiceflow.chat.proactive.clear();
-    window.voiceflow.chat.proactive.push(
-      { type: 'text', payload: { message: proactiveMessage } }
-    );
-  }).catch((err) => console.error('Widget load failed:', err));
-};
-script.src = "https://cdn.voiceflow.com/widget/bundle.mjs";
-document.head.appendChild(script);
+  (function(d, t) {
+      var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+      v.onload = function() {
+        window.voiceflow.chat.load({
+          verify: { projectID: '67436de76414aa7986e35112' },
+          url: 'https://general-runtime.voiceflow.com',
+          versionID: 'production',
+          assistant:  {
+            description: widgetDescription,
+            extensions: [FormExtension]
+          },
+          launch: {
+            event: { type: "launch", payload: { browser_url: window.location.href } }
+      }
+        });
+      }
+      v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+  })(document, 'script');
