0f9caad1cc6d8c07c4d87408ef5a6d57ae70a9fd
kristoman-rikardo <kristofferswik@gmail.com>
2025-01-29 15:40:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
- (function(d, t) {
-      var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
-      v.onload = function() {
-        window.voiceflow.chat.load({
-          verify: { projectID: '67436de76414aa7986e35112' },
-          url: 'https://general-runtime.voiceflow.com',
-          versionID: 'production',
-          assistant: {
-             extensions: FormExtension,
-             banner: {
-               description: widget_Description,
-                     },
-             inputPlaceholder: input_Placeholder
-                   },
-         launch: {
-            event: { type: "launch", payload: { browser_url: window.location.href } }
-      }
-        });
-      };
-      v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; 
-      v.type = "text/javascript"; 
-      s.parentNode.insertBefore(v, s);
-  })(document, 'script');
+(function(d, t) {
+    var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+    v.onload = function() {
+      window.voiceflow.chat.load({
+        verify: { projectID: '67436de76414aa7986e35112' },
+        url: 'https://general-runtime.voiceflow.com',
+        versionID: 'production'
+      });
+    }
+    v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+})(document, 'script');
