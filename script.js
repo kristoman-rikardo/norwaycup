615553ca832920dbff95c3b9e50c9532ee1d49c2
kristoman-rikardo <kristofferswik@gmail.com>
2025-02-21 23:06:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ let browser_url = window.location.href;
  if (browser_url.includes('/?sprak=3')) {
 widgetDescription = "I'm quite smart, but I need a couple of seconds to think it through⚽ I'm still in training and may make some minor mistakes.";
 inputPlaceholder = "Message...";
-widgetStylesheet = "buttonlabels/en";
+widgetStylesheet = "https://kristoman-rikardo.github.io/buttonlabels/en.css";
 } else {
 widgetDescription = "Jeg er ganske smart, men trenger et par sekunder for å tenke meg om⚽ Jeg er fortsatt under opplæring, og kan derfor gjøre små feil.";
 inputPlaceholder = "Skriv melding...";
-widgetStylesheet = "buttonlabels/en";
+widgetStylesheet = "https://kristoman-rikardo.github.io/buttonlabels/no.css";
 }
 
 // Last inn Chat-widget
